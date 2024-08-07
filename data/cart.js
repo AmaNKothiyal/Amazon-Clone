8177ad419993379995075c648d3729a9a9a51537
AmanKothiyal <amankothiyal2003@gmail.com>
2024-08-07 21:07:31 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+
+[{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity:2
   },{
@@ -6,6 +8,10 @@ export let cart = [{
     quantity:1
     
   }];
+
+  function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+  }
 
 export function addToCart(productId){
     let matchingItem;
@@ -24,7 +30,7 @@ export function addToCart(productId){
             quantity:1
         });
     }
-    
+    saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -33,5 +39,8 @@ export function removeFromCart(productId){
          if(cartItem.productId!==productId)
             newCart.push(cartItem);
     });
+    
     cart=newCart;
+
+    saveToStorage();
 }
